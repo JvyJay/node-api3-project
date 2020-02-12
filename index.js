b2466fc59c4b56c03fd393db527b544ca98e412c
Jadon Guzman <jjkolet25@tutanota.com>
2020-02-12 15:50:42 -0800

    inital-commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const postRouter = require('./posts/postRouter');
+
+const server = express();
+
+server.use(express.json());
+
+server.use(helmet());
+
+server.use('/api/posts', postRouter);
+
+module.exports = server;
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,30 @@
 const express = require('express');
-
+const Posts = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.find(req.query)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        msg: "Can't retrieve Posts"
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Posts.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ msg: 'Post not found' });
+    }
+  });
 });
 
 router.delete('/:id', (req, res) => {
